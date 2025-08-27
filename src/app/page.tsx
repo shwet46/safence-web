31cd792c36dc245fe12f3cd0dc2da5d1e1f1d222
Shweta <beherashweta54@gmail.com>
2025-08-27 17:33:28 +0530

    changes

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-4xl font-bold text-center text-fuchsia-700">Welcome to Safence</h1>
+    </div>
+  );
+}
